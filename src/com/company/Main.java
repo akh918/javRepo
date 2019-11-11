8de1bfed99b5e0710cc6bb375dbedb4769969d67
ac <praveen.pavan0509@gmail.com>
2019-11-11 19:34:58 +0530

    second coomit with some addition

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,8 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("hello welcome to git world");
+        int a=10 ,b = 20;
+        System.out.println(a+b);
+
     }
 }
